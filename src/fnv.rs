1ff4e5ee5d64760c5f3f9c5174f66d8eca1e2615
Ryan Lopopolo <rjl@hyperbo.la>
2020-07-06 20:48:16 -0700

    more inline

--- src/fnv.rs
+++ src/fnv.rs
@@ -10,6 +10,8 @@ pub struct Fnv1a32BuildHasher;
 impl BuildHasher for Fnv1a32BuildHasher {
     type Hasher = Fnv1a32;
 
+    #[inline]
+    #[must_use]
     fn build_hasher(&self) -> Self::Hasher {
         Self::Hasher::default()
     }
@@ -19,23 +21,28 @@ pub struct Fnv1a32(u32);
 
 impl Fnv1a32 {
     #[inline]
+    #[must_use]
     pub fn new() -> Self {
         Self::default()
     }
 
     #[inline]
+    #[must_use]
     pub fn with_seed(seed: u32) -> Self {
         Self(seed)
     }
 }
 
 impl Default for Fnv1a32 {
+    #[inline]
+    #[must_use]
     fn default() -> Self {
         Self(FNV1_32A_INIT)
     }
 }
 
 impl fmt::Debug for Fnv1a32 {
+    #[inline]
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "Fnv1a32 {{}}")
     }
@@ -51,11 +58,14 @@ impl Hasher for Fnv1a32 {
     }
 
     #[inline]
+    #[must_use]
     fn finish(&self) -> u64 {
         self.0 as u64
     }
 }
 
+#[inline]
+#[must_use]
 pub fn hash<T: AsRef<[u8]>>(data: T) -> u64 {
     let mut hasher = Fnv1a32::default();
     hasher.write(data.as_ref());
