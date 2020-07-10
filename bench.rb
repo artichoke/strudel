8ea5442abaa0c14d223fc4187992f4dd7e038137
Ryan Lopopolo <rjl@hyperbo.la>
2020-07-09 18:28:57 -0700

    rubocop bench.rb

--- bench.rb
+++ bench.rb
@@ -1,4 +1,5 @@
 #! /usr/bin/env ruby
+# frozen_string_literal: true
 
 require 'benchmark'
 
@@ -14,10 +15,10 @@ count = 50_000_000
 indexes = count.times.map { rand(1000) }
 
 h2 = {}
-Benchmark.bm do |x| 
-  x.report("values") { 500_000.times { h.values } }
-  x.report("keys  ") { 500_000.times { h.keys } }
-  x.report("find  ") { indexes.each { |i| h[i] } }
-  x.report("insert") { indexes.each { |i| h2[i] = i.to_s } }
-  x.report("delete") { indexes.each { |i| h.delete(i) } }
+Benchmark.bm do |x|
+  x.report('values') { 500_000.times { h.values } }
+  x.report('keys  ') { 500_000.times { h.keys } }
+  x.report('find  ') { indexes.each { |i| h[i] } }
+  x.report('insert') { indexes.each { |i| h2[i] = i.to_s } }
+  x.report('delete') { indexes.each { |i| h.delete(i) } }
 end
