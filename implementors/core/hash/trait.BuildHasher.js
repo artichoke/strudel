43659fa9bf2fe43b29fcefdf10a3041959fee924
artichoke-ci <ci@artichokeruby.org>
2021-01-12 13:12:47 +0000

    deploy: 25767f14d26e1099b0bded05cb3935ab8ea50a97

--- implementors/core/hash/trait.BuildHasher.js
+++ implementors/core/hash/trait.BuildHasher.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["strudel"] = [{"text":"impl BuildHasher for StBuildHasher","synthetic":false,"types":[]},{"text":"impl BuildHasher for Box&lt;StBuildHasher&gt;","synthetic":false,"types":[]}];
+implementors["strudel"] = [{"text":"impl BuildHasher for StBuildHasher","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
