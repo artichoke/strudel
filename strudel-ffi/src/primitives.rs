75cb223cf987dc473c05c82b7e0cf915a75fe852
Ryan Lopopolo <rjl@hyperbo.la>
2022-02-08 22:39:28 -0800

    address clippy lints, add cargo metadata

--- strudel-ffi/src/primitives.rs
+++ strudel-ffi/src/primitives.rs
@@ -41,7 +41,7 @@ impl st_data_t {
     /// This `st_data_t` must point to a C String allocation.
     #[inline]
     #[must_use]
-    pub unsafe fn as_const_c_char(&self) -> *const c_char {
+    pub unsafe fn as_const_c_char(self) -> *const c_char {
         self.inner as *const _
     }
 
